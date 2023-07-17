0c776605048c752916bfe20da30704e05f2b4ac7
mobo <morgan.bohn@pc-scol.fr>
2023-07-17 14:44:25 +0200

    [TECH] reformat with black/isort

--- api_demo/main.py
+++ api_demo/main.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
 from __future__ import print_function
+
+import os
+from pprint import pprint
+
 import swagger_client
 from swagger_client.rest import ApiException
-from pprint import pprint
-import os
 
 # Configuration de l'url et de l'authentification
 configuration = swagger_client.Configuration()
 configuration.host = "https://ins.hotfix.pc-scol.fr/api/v5/ins"
-configuration.api_key_prefix['Authorization'] = 'Bearer'
-configuration.api_key['Authorization'] = os.environ['BEARER_TOKEN']
+configuration.api_key_prefix["Authorization"] = "Bearer"
+configuration.api_key["Authorization"] = os.environ["BEARER_TOKEN"]
+
 
 # Monkey patch auth_settings (bug: https://github.com/swagger-api/swagger-codegen/issues/10060)
 def auth_settings(self):
-        return {
-            'idTokenAuth':
-                {
-                    'type': 'http',
-                    'in': 'header',
-                    'key': 'Authorization',
-                    'value': self.get_api_key_with_prefix('Authorization')
-                },
-        }
+    return {
+        "idTokenAuth": {
+            "type": "http",
+            "in": "header",
+            "key": "Authorization",
+            "value": self.get_api_key_with_prefix("Authorization"),
+        },
+    }
+
+
 swagger_client.Configuration.auth_settings = auth_settings
 
 # Création de l'instance de l'API
 api_instance = swagger_client.ApprenantsApi(swagger_client.ApiClient(configuration))
-code_structure = 'ETAB00' # str | Le code de l'établissement
-code_apprenant = '20220117' # str | Le code Pegase de l'apprenant
+code_structure = "ETAB00"  # str | Le code de l'établissement
+code_apprenant = "20220117"  # str | Le code Pegase de l'apprenant
 
 try:
     # Chercher les données d'un apprenant
@@ -35,4 +39,3 @@ try:
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling ApprenantsApi->lire_apprenant: %s\n" % e)
-
